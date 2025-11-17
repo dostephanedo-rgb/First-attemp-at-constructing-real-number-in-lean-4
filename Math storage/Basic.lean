3d2b7597a707789b6ce6add6a1d3b00cc7919629
Stephane <dostephanedo@gmail.com>
2025-11-17 13:41:45 +1100

    Ading: Natural number definition theorem S(n) not equal 0

--- Math storage/Basic.lean
+++ Math storage/Basic.lean
@@ -1,1 +1,10 @@
-def hello := "world"
+inductive NaturalNumber: Type where
+|Zero : NaturalNumber
+|Successor : NaturalNumber → NaturalNumber
+notation "N" => NaturalNumber
+instance : Zero NaturalNumber where
+  zero := NaturalNumber.Zero
+def s := NaturalNumber.Successor
+theorem SuccessorNotEqualZero ( n : N ) : s n ≠ 0 := by
+intro h
+contradiction
